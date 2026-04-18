3d4bb98e04f696b9fb5cc01147f30b2f88eb8ac2
ryan taira <ryan_koji@hotmail.com>
2026-04-18 17:02:20 -0300

    feat: metodo para adicionar tempo

--- Microondas.Domain/Microondas.cs
+++ Microondas.Domain/Microondas.cs
@@ -4,4 +4,12 @@ namespace Microondas.Domain;
 public class Microondas
 {
 
+    public int Seconds { get; private set; }
+
+    public void AdicionarTempo(int seconds)
+    {
+        if (seconds < 1 || seconds > 120) throw new Exception("Valor do tempo fora do limite permitido");
+
+        Seconds = seconds;
+    }
 }
--- Microondas.Test.Domain/MicroondasTest.cs
+++ Microondas.Test.Domain/MicroondasTest.cs
@@ -2,5 +2,26 @@ namespace Microondas.Test.Domain;
 
 public class MicroondasTest
 {
-    
+    [Fact]
+    public void AcionarTempo_ValorDentroTempoLimite()
+    {
+        var microondas = new Microondas.Domain.Microondas();
+
+        microondas.AdicionarTempo(1);
+
+        Assert.Equal(1, microondas.Seconds);
+    }
+
+    [Theory]
+    [InlineData(121)]
+    [InlineData(500)]
+    [InlineData(0)]
+    [InlineData(-1)]
+    public void AcionarTempo_ValorAcimaDoLimite(int segundosForaDoIntervalo)
+    {
+        var microondas = new Microondas.Domain.Microondas();
+        Assert.Throws<Exception>(() => microondas.AdicionarTempo(segundosForaDoIntervalo));
+
+    }
+
 }
